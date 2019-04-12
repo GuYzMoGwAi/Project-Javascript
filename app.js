f04127649210f8c05b06bb8f459a4eb0ad28be0d
Blaud <blaudlewis@hotmail.fr>
2019-04-12 11:22:30 +0200

    Amélioration hit box et autre

--- app.js
+++ app.js
@@ -2,37 +2,40 @@ var game = new Phaser.Game(800, 800);
 var vitesse = 600;
 
 var dodger = {
-    preload: function(){
+    preload: function () {
         //Chargement image et le son.
-        game.load.image('ciel','ressources/images/ciel.png');
-        game.load.image('troll','ressources/images/troll.png');
-        game.load.image('YimPhilippe','ressources/images/YimPhilippe.png');
+        game.load.image('ciel', 'ressources/images/ciel.png');
+        game.load.image('troll', 'ressources/images/troll.png');
+        game.load.image('YimPhilippe', 'ressources/images/YimPhilippe.png');
     },
-    create: function() {
+    create: function () {
         //Setup + affichage.
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         game.add.sprite(0, 0, 'ciel');
 
-        this.player = game.add.sprite(100,100, 'troll');
+        this.player = game.add.sprite(400, 600, 'troll'); //Emplacement du player
         this.player.anchor.set(0.5)
         game.physics.arcade.enable(this.player);
 
-        this.cursors = game.input.keyboard.createCursorKeys(); 
-        
+        this.cursors = game.input.keyboard.createCursorKeys();
+
+
         this.YimPhilippes = game.add.group();
 
         this.timer = game.time.events.loop(200, this.ajouterUnMechant, this);
 
         this.score = 0;
-        this.labelScore = game.add.text(20, 20, "0", {font: "30px Arial", fill: "#fff"}); //Emplacement du text, personnalisation(taille,color etc).
-
+        this.labelScore = game.add.text(20, 20, "0", {
+            font: "30px Arial",
+            fill: "#fff"
+        }); //Emplacement du text, personnalisation(taille,color etc). 
 
 
     },
-    update : function() {
+    update: function () {
         //logique du jeux
-        game.physics.arcade.overlap(this.player, this.YimPhilippes, this.restartGame, null, this); // Permet de créer les collisions (entre le player et les mechan
+        game.physics.arcade.collide(this.player, this.YimPhilippes, this.restartGame, null, this); // Permet de créer les collisions (entre le player et les mechants) et de faire redémarrer la partie.
         
         this.player.body.velocity.x = 0; //Permet de remettre velocity x a zero(Update ce recharge 60 f/m.)
         this.player.body.velocity.y = 0; //Permet de remettre velocity y a zero(Update ce recharge 60 f/m.)
@@ -52,11 +55,11 @@ var dodger = {
             this.restartGame();
         }
     },
-    restartGame: function() { 
+    restartGame: function () {
         game.state.start('dodger');
-        alert('GAME OVER. Merci pour votre participation. Vous pouvez rejouer, ou allé voir la documentation !!'); 
+        alert('GAME OVER. Merci pour votre participation. Vous pouvez rejouer, ou aller voir la DOC !!');
     },
-    
+
     ajouterUnMechant() {
         var position = Math.floor(Math.random() * 770) + 1; //Pour arrondir (-50 pour le mechant)
         var YimPhilippe = game.add.sprite(position, -30, 'YimPhilippe'); //détermine la ou spawn les méchants.
@@ -71,6 +74,8 @@ var dodger = {
 
         YimPhilippe.checkWorldBounds = true; //vérifier si les mechant sont dedans ou en dehort du "monde".
         YimPhilippe.outOfBoundsKill = true; // Si il est en dehort du "monde" il meurt.
+
+        
     }
 };
 
